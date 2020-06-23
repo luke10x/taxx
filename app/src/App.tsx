573effc9ef1fac57d0f896e5ce644392451fc0a6
Luke 10x <spam@luke10x.dev>
2020-06-23 03:47:08 +0100

    🚧 demo of the app talking to the sync server

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [syncStatus, setSyncStatus] = useState<String>("Loading...");
+
+  useEffect(() => {
+    fetch("http://localhost:4000/")
+    .then((response: Response) => response.text())
+    .then((text: String) => {
+      setSyncStatus(text);
+    })
+    .catch((error: Error) => {
+      console.error("Failed to fetch", error);
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +24,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <dl>
+          <dt>Sync status:</dt>
+          <dd>{syncStatus}</dd>
+        </dl>
         <a
           className="App-link"
           href="https://reactjs.org"
